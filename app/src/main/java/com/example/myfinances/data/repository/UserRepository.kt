9e25e93fe6575c53dade6ebab314f6d493adf59a
Victor Laurentino do Nascimento <53540002+PitzTech@users.noreply.github.com>
2024-06-06 23:41:01 -0300

    fix userrepository getuserbyemail

--- app/src/main/java/com/example/myfinances/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/myfinances/data/repository/UserRepository.kt
@@ -10,4 +10,6 @@ class UserRepository(private val userDao: UserDao) {
     fun insert(user: User) = userDao.insert(user)
     fun update(user: User) = userDao.update(user)
     fun delete(user: User) = userDao.delete(user)
+
+    suspend fun getUserByEmail(email: String): User? = userDao.getUserByEmail(email)
 }
