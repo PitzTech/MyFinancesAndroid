6ab5fda973c1916eb161e13e53a8bc2fba087f3c
Victor Laurentino do Nascimento <53540002+PitzTech@users.noreply.github.com>
2024-06-06 23:40:24 -0300

    apply typeconverter

--- app/src/main/java/com/example/myfinances/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/myfinances/data/database/AppDatabase.kt
@@ -1,11 +1,24 @@
 package com.example.myfinances.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myfinances.data.dao.*
 import com.example.myfinances.data.model.*
+import androidx.room.TypeConverters
 
-@Database(entities = [User::class, Preference::class, Bank::class, CreditCard::class, Category::class, Transaction::class], version = 1)
+@Database(entities = [
+    User::class,
+    Preference::class,
+    Bank::class,
+    CreditCard::class,
+    Category::class,
+    Transaction::class
+                     ],
+    version = 1
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun preferenceDao(): PreferenceDao
@@ -13,4 +26,21 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun creditCardDao(): CreditCardDao
     abstract fun categoryDao(): CategoryDao
     abstract fun transactionDao(): TransactionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "myfinances_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
